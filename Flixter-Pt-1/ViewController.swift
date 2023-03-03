84f65ee8a765b79d3d3278c52f011d5c74c63ee4
KennedyParks <kennedyjparks@gmail.com>
2023-03-03 00:10:23 -0600

    Initial Commit

---
+++ Flixter-Pt-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixter-Pt-1
+//
+//  Created by Kennedy Parks on 3/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
